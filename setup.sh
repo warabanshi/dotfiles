3a02245c3eb907733b8e009415d4f91f3ce81683
warabanshi <warabanshi@gmail.com>
2021-11-03 01:26:15 +0900

    Added pyenv setup

--- setup.sh
+++ setup.sh
@@ -3,26 +3,52 @@
 set -ue -o pipefail
 
 BASE_DIR=`pwd`
+DEST_DIR=~/
+
+cleanup() {
+    rm -rf ${BASE_DIR}/dotfiles
+}
 
 init() {
-    trap 'rm -rf ${BASE_DIR}/dotfiles' EXIT
+    trap cleanup EXIT
     git clone https://github.com/warabanshi/dotfiles.git
 }
 
 common() {
     cd dotfiles
     mkdir ~/tmp-dotfiles
-    cp -r files/* ~/tmp-dotfiles
+    find ./files -maxdepth 1 -type f -exec cp {} ${DEST_DIR} \;
+    cp -r ./files/.vim ${DEST_DIR}
 
     cd $OLDPWD
+
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 }
 
 debian() {
     common
+
+    # setup pyenv
+    sed -Ei -e '/^([^#]|$)/ {a \
+    export PYENV_ROOT="$HOME/.pyenv"
+    a \
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    a \
+    ' -e ':a' -e '$!{n;ba};}' ~/.profile
+    echo 'eval "$(pyenv init --path)"' >>~/.profile
+
+    echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 }
 
 opensuse() {
     common
+
+    # setup pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+    echo 'eval "$(pyenv init --path)"' >> ~/.profile
+
+    echo 'if command -v pyenv >/dev/null; then eval "$(pyenv init -)"; fi' >> ~/.bashrc
 }
 
 
